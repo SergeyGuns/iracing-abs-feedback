48895cbd4796f7ee6e44e454193eaf3e89678229
Ganin Sergey <mcheguevara2@gmail.com>
2024-12-30 15:59:29 +0300

    fix

--- index.js
+++ index.js
@@ -1,52 +1,59 @@
 const { IRacingSDK } = require("irsdk-node");
 const { TELEMETRY } = require("./telemetry.constants");
-const { SerialPort } = require('serialport');
-const TIMEOUT = Math.floor((1 / 60) * 1000); // 60fps
+// const { SerialPort } = require('serialport');
+const TIMEOUT = Math.floor((1 / 30) * 1000); // 60fps
 
-const portName = await findComPortsByName('Arduino')
-const port = new SerialPort({ path: portName, baudRate: 9600 });
-function sendABSValue(value) {
-    const buffer = Buffer.from([value]);
-    port.write(buffer);
-}
-
-port.on('error', function (err) {
-    console.error('Error:', err.message);
-});
-function loop() {
-    if (sdk.waitForData(TIMEOUT)) {
-        const session = sdk.getSessionData();
-        const telemetry = sdk.getTelemetry();
-        console.log(telemetry[TELEMETRY.BrakeABSactive])
-        sendABSValue(telemetry[TELEMETRY.BrakeABSactive])
-        loop(sdk);
-    } else {   
-        // Disconnected.
+let sdk;
+async function init () {
+    // const portName = await findComPortsByName('Arduino')[0]
+    // const port = new SerialPort({ path: 'COM3', baudRate: 9600 });
+    function sendABSValue(value) {
+        const buffer = Buffer.from([value]);
+        // port.write(buffer);
     }
-}
-
-
-
-if (await IRacingSDK.IsSimRunning()) {
-    const sdk = new IRacingSDK();
-    if (await sdk.ready()) {
-        sdk.autoEnableTelemetry = true;
-        loop(sdk);
+    
+    if (await IRacingSDK.IsSimRunning()) {
+        sdk = new IRacingSDK();
+        if (await sdk.ready()) {
+            sdk.autoEnableTelemetry = true;
+            loop(sdk);
+        }
     }
+    function loop() {
+        if (sdk.waitForData(TIMEOUT)) {
+            const session = sdk.getSessionData();
+            const telemetry = sdk.getTelemetry();
+            console.log(telemetry[TELEMETRY.BrakeABSactive])
+            sendABSValue(telemetry[TELEMETRY.BrakeABSactive])
+            loop(sdk);
+        } else {   
+            loop(sdk);
+        }
+    }
+    // port.on('error', function (err) {
+    //     console.error('Error:', err.message);
+    // });
+    
+    
+    
+    
+    // async function findComPortsByName(namePattern) {
+    //     const ports = await SerialPort.list();
+    //     return ports.filter(port => port.manufacturer && port.manufacturer.includes(namePattern));
+    // }
+    
+    // findComPortsByName('Arduino')
+    //     .then((ports) => {
+    //         console.log(`Найдено ${ports.length} портов:`);
+    //         return ports.forEach((port) => {
+    //             console.log(`${port.comName}: ${port.manufacturer}`);
+    //         });
+    //     })
+    //     .catch((err) => {
+    //         console.error('Ошибка при поиске портов:', err);
+    //     });
 }
 
-async function findComPortsByName(namePattern) {
-    const ports = await SerialPort.list();
-    return ports.filter(port => port.manufacturer && port.manufacturer.includes(namePattern));
-}
+init();
 
-findComPortsByName('Arduino')
-    .then((ports) => {
-        console.log(`Найдено ${ports.length} портов:`);
-        return ports.forEach((port) => {
-            console.log(`${port.comName}: ${port.manufacturer}`);
-        });
-    })
-    .catch((err) => {
-        console.error('Ошибка при поиске портов:', err);
-    });
+
